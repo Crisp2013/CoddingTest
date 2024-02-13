eef9001bd205c02e3b3f3c21619c62da3f10c426
wintiger0222 <0xodbs02@naver.com>
2024-02-13 16:02:53 +0900

    solve 14002

--- python3/14002.py
+++ python3/14002.py
@@ -1,29 +1,39 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-temp = []
-points = -1
+lists = [[a[0],1]]
+lists_count = 1
+for i in range(1,n):
 
-for i in range(n):
-    # print(temp,points)
-    if points == -1:
-        temp.append(a[i])
-        points = points + 1
-        continue
-    if temp[points]<a[i]:
-        temp.append(a[i])
-        points = points + 1
-        continue
-    if temp[0]<a[i]:
-        temp[0]=a[i]
-        points = 0
-        continue
-
-    for j in range(points-1,-1,-1):
-        if temp[j]>a[i]:
-            temp[j+1]=a[i]
-            points = j + 1
+    # print(lists)
+    for task in range(lists_count+1):
+        if task == lists_count:
+            lists.append([a[i],1])
+            lists_count = lists_count + 1
             break
+        
+        if a[i] > lists[task][0]:
+            lists.append([a[i],lists[task][1]+1])
+            lists_count = lists_count + 1
+            break
+            
+    lists.sort(key=lambda x:-x[1])
 
-print(points+1)
-print(str(temp).replace(',','').replace(']','').replace('[',''))
+print(lists[0][1])
+
+k = lists[0][1]
+m = lists[0][0]
+t = [lists[0][0]]
+for i in lists:
+    if i[1] == k-1:
+        if i[0] < m:
+            t.append(i[0])
+            k = k - 1
+            m= i[0]
+t.reverse()
+print(str(t).replace(",","").replace("[","").replace("]",""))
+
+
+
+
+
