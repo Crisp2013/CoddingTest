37ceb12dc6b6990ee02e9d44cf6d3c2539e85405
wintiger0222 <0xodbs02@naver.com>
2024-02-26 15:51:20 +0900

    Solve 15654 15657 15666 15654 - N과 M (5) 15657 - N과 M (8) 15666 - N과 M (12)

---
+++ python3/15657.py
@@ -0,0 +1,17 @@
+from collections import deque
+n, m = list(map(int, input().split()))
+a = sorted(list(map(int, input().split())))
+
+answer = deque()
+for i in a:
+    answer.append([i])
+
+for i in range(1,m):
+    temp_answer = []
+    while len(answer)!=0:
+        temp = answer.popleft()
+        for j in a:
+            if temp[-1] <= j:
+                temp_answer.append(temp+[j])
+    answer = deque(temp_answer.copy())
+print(str(list(answer)).replace("], [","\n").replace(", "," ").replace("]","").replace("[",""))
